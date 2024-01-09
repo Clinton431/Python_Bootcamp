be72128da29467807aacc803dc67038e2b7bb127
Clinton431 <clintonnyakoe@gmail.com>
2024-01-09 12:23:41 +0300

    New updates

---
+++ modules.py
@@ -0,0 +1,12 @@
+# module = a file containig python code. May contain functions, classes, etc.
+# used with modular programming, which is to separate a program into parts
+
+# import messages as msg
+# from messages import hello, bye
+# from messages import *
+
+# hello()
+# bye()
+
+help("modules")
+
